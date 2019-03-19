8a86b61856715d84b796027c9c57297c300910f4
dcc0 <ivangavr777@gmail.com>
2019-03-19 21:00:34 +0300

    Update Class_Printing.php

--- chat/lib/view/Class_Printing.php
+++ chat/lib/view/Class_Printing.php
@@ -7,15 +7,21 @@ require_once __DIR__ . '/../../conf/Class_Connect.php';
 require_once './Class_ListChannels.php';
 //Класс вывода
 require_once 'Class_Cycle.php';
+
+
+
 //Проверим имя канала через GET
 if(!isset($_GET['channel']) || empty($_GET['channel']))
 $_GET['channel'] = 'chat';
+
+
+
 class Printing
   {
     protected $connection = null;
     protected $fetch;
     protected $my_channel;
-    protected $new_cyle;
+	protected $new_cyle;
     public function __construct($mysql, $new_cyle)
       {
         $connection = $mysql->getConnection();
@@ -26,6 +32,7 @@ class Printing
         $this->connection = $mysql->getConnection();
 		$this->new_cyle=$new_cyle;
       }
+	
     /*Запрашиваем последние 15 сообщений*/
     private function fetchingOut($last_message_id, $channel)
       {
@@ -35,29 +42,28 @@ class Printing
             $stmt->bind_param('ds',  $last_message_id, $channel);
             $stmt->execute();
             $result=$stmt->get_result();
-	
+            return  $result;
+            
 	//В комментарии ниже историческое название метода
 	//return $this->new_cyle->startCycleFetchAssocAndReturnJsonFromClassChatUsingMethodPrintingOutAndDontBeSoSeriousHaveANiceDay( $result);
-	
-	    return $this->new_cyle->FethcAssocArrayReturnJson($result);
+		
 		
     }
+	
     public function printingOut($last_id, $channel)
       {
         $data = $this->fetchingOut($last_id, $channel);
-           return json_encode($data);
+        //Непосредственно цикл вывода в другом класса. Class_Cycle.php        
+         return json_encode($this->new_cyle->FethcAssocArrayReturnJson($data));
       }
   }
 //Коннект к базе
 $connection_to = new Connect();
-
 //Новый контекст для цикла
 $new_cyle = new Cycle($array_for_cycle, $mybject);
-
 //Вывод
 $data          = new Printing($connection_to, $new_cyle);
-
-//Запрос последних сообщний. Параметр номер последненего сообщения
+//Запрос последних сообщний. Парамерт номер последненего сообщения
 $newarr_data=$data->printingOut($_GET['last_message_id'],trim($_GET['channel']));
 print $newarr_data;
 ?>
