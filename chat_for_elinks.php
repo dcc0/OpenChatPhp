857a8ff499ed7f3b2c0f029d867f532f7b316a80
dcc0 <ivangavr777@gmail.com>
2023-06-14 18:21:34 +0300

    Update chat_for_elinks.php

--- chat_for_elinks.php
+++ chat_for_elinks.php
@@ -1,50 +1,45 @@
-//Если не работает JavaScript, для можно использовать данный скрипт для чата.
+Если не работает JavaScript, для можно использовать данный скрипт для чата. После 30 сообщений файл очищается.
 <form  method="POST" action="chat2.php"> 			
 	<input type="text" id="text" name="text" size="100%"  maxlength="500"><br/>	
 	<input  type="submit" name="submit" value="Send">
 </form>
-<?php
 
+<?php
+//Обрежем запрос POST до 500 символов
+$_POST['text']=mb_substr($_POST['text'], 0,500, 'UTF-8');
 $str2 = $_POST['text'];
+//Запишем сообщение в файл
+
 if (isset($_POST['submit']) && !empty($_POST['text']) && $_POST['text'] !='') {
 	$fd = fopen("chat.txt", 'a') or die("не удалось создать файл");
 	fseek($fd, 0, SEEK_END);
 	fwrite($fd, "\r\n");
 	fwrite($fd, $str2);
 	fclose($fd);
+	print "<meta http-equiv=\"refresh\"        content=\"1\">";
 }
 
-
+//Читаем файл в массив text для подсчёта строк
 $fd = fopen("chat.txt", 'r') or die("не удалось открыть файл");
-
 while(!feof($fd))
 {
 	 $str = htmlentities(fgets($fd));
-	 if ($str != "" && strlen($str) >  2)
+	 if ($str != "" && strlen($str) >  2) {
 	 $text[] = $str;
+ }
 }
 fclose($fd);
-//print_r($text);
+#print_r($text);
 
-if (count($text) > 25)  {
-
+//Очистим файл, если сообщений больше 30
+if (count($text) > 30)  {
 	$f = fopen('chat.txt', 'w');
 	fclose($f);
-	$i=1;
-	$fd = fopen("chat.txt", 'a') or die("не удалось создать файл");
-	while($i != 24) 
-{
-	fseek($fd, 0, SEEK_END);
-    	fwrite($fd, "\r\n");
-    	fwrite($fd, $text[$i]);
-        fwrite($fd, "\r\n");
-    	$i++;
-}
-fclose($fd);
 }
 
-print "<meta http-equiv=\"refresh\"        content=\"[значение]\">";
-$text=array_reverse($text);
-foreach ( $text as $val)
-echo $val . '<br>';
+
+//Вывод сообщений
+	$text=array_reverse($text);
+	foreach ( $text as $value) 
+	echo $value . '<br>';
 ?>
