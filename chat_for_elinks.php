96607be138bbaafee1b21efb54ad0d284eb69184
dcc0 <ivangavr777@gmail.com>
2023-06-14 16:29:04 +0300

    Update chat_for_elinks.php

--- chat_for_elinks.php
+++ chat_for_elinks.php
@@ -7,11 +7,11 @@
 
 $str2 = $_POST['text'];
 if (isset($_POST['submit']) && !empty($_POST['text']) && $_POST['text'] !='') {
-$fd = fopen("chat.txt", 'a') or die("не удалось создать файл");
-fseek($fd, 0, SEEK_END);
-fwrite($fd, "\r\n");
-fwrite($fd, $str2);
-fclose($fd);
+	$fd = fopen("chat.txt", 'a') or die("не удалось создать файл");
+	fseek($fd, 0, SEEK_END);
+	fwrite($fd, "\r\n");
+	fwrite($fd, $str2);
+	fclose($fd);
 }
 
 
@@ -19,9 +19,9 @@ $fd = fopen("chat.txt", 'r') or die("не удалось открыть файл
 
 while(!feof($fd))
 {
-    $str = htmlentities(fgets($fd));
-    if ($str != "" && strlen($str) >  2)
-    $text[] = $str;
+	 $str = htmlentities(fgets($fd));
+	 if ($str != "" && strlen($str) >  2)
+	 $text[] = $str;
 }
 fclose($fd);
 //print_r($text);
@@ -35,20 +35,16 @@ if (count($text) > 25)  {
 	while($i != 24) 
 {
 	fseek($fd, 0, SEEK_END);
-    fwrite($fd, "\r\n");
-    fwrite($fd, $text[$i]);
+    	fwrite($fd, "\r\n");
+    	fwrite($fd, $text[$i]);
         fwrite($fd, "\r\n");
-    $i++;
+    	$i++;
 }
 fclose($fd);
-
 }
-
 
 print "<meta http-equiv=\"refresh\"        content=\"[значение]\">";
 $text=array_reverse($text);
 foreach ( $text as $val)
 echo $val . '<br>';
-
 ?>
-
